34d006f4121f8a6d7e0a99666a02387364d8e4ea
Marcus Holl <marcus.holl@sap.com>
2018-11-30 11:37:22 +0100

    Relocate node test in order to provide other tests later on

--- mta-archive-builder/run.sh
+++ mta-archive-builder/run.sh
@@ -1,13 +1,13 @@
 #/bin/bash
 
-export BUILD_ARTIFACT=dummy.mtar
+export NODE_BUILD_ARTIFACT=node/dummy.mtar
 
 docker build -t mta . || { echo "[ERROR] Failed to build mta docker image."; exit 1; }
 cd test
-docker run -it --rm -v `pwd`:/project mta mtaBuild --build-target NEO --mtar ${BUILD_ARTIFACT} build
+docker run -it --rm -v `pwd`/node:/project mta mtaBuild --build-target NEO --mtar ${NODE_BUILD_ARTIFACT} build
 
 [ $? != 0 ] && { echo "Cannot run mta docker image."; exit 1; }
 
-[ -f "${BUILD_ARTIFACT}" ] || { echo "Expected build result \"${BUILD_ARTIFACT}\" not found."; exit 1; }
+[ -f "${NODE_BUILD_ARTIFACT}" ] || { echo "Expected build result \"${NODE_BUILD_ARTIFACT}\" not found."; exit 1; }
 
 cd -
