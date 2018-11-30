1b4d7f3fbe923eaa015a56b53c54d4e4028669cf
Marcus Holl <marcus.holl@sap.com>
2018-11-30 12:11:28 +0100

    test maven build through mta

--- mta-archive-builder/run.sh
+++ mta-archive-builder/run.sh
@@ -2,11 +2,14 @@
 
 export BUILD_ARTIFACT=dummy.mtar
 export NODE_TEST_FOLDER=test/node
+export MAVEN_TEST_FOLDER=test/maven
 
 docker build -t mta . || { echo "[ERROR] Failed to build mta docker image."; exit 1; }
 
 docker run -it --rm -v `pwd`/${NODE_TEST_FOLDER}:/project mta mtaBuild --build-target NEO --mtar ${BUILD_ARTIFACT} build
-
 [ $? != 0 ] && { echo "Cannot run mta docker image."; exit 1; }
-
 [ -f "${NODE_TEST_FOLDER}/${BUILD_ARTIFACT}" ] || { echo "Expected build result \"${NODE_TEST_FOLDER}/${BUILD_ARTIFACT}\" not found."; exit 1; }
+
+docker run -it --rm -v `pwd`/${MAVEN_TEST_FOLDER}:/project mta mtaBuild --build-target NEO --mtar ${BUILD_ARTIFACT} build
+[ $? != 0 ] && { echo "Cannot run mta docker image."; exit 1; }
+[ -f "${MAVEN_TEST_FOLDER}/${BUILD_ARTIFACT}" ] || { echo "Expected build result \"${MAVEN_TEST_FOLDER}/${BUILD_ARTIFACT}\" not found."; exit 1; }
