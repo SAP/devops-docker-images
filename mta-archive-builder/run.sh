0782e8c44a2ee9403a7a2903ddfa2b2bf351d936
Marcus Holl <marcus.holl@sap.com>
2018-11-30 11:43:34 +0100

    Generalize test exection

--- mta-archive-builder/run.sh
+++ mta-archive-builder/run.sh
@@ -1,13 +1,12 @@
 #/bin/bash
 
-export NODE_BUILD_ARTIFACT=node/dummy.mtar
+export BUILD_ARTIFACT=dummy.mtar
+export NODE_TEST_FOLDER=test/node
 
 docker build -t mta . || { echo "[ERROR] Failed to build mta docker image."; exit 1; }
-cd test
-docker run -it --rm -v `pwd`/node:/project mta mtaBuild --build-target NEO --mtar ${NODE_BUILD_ARTIFACT} build
+
+docker run -it --rm -v `pwd`/${NODE_TEST_FOLDER}:/project mta mtaBuild --build-target NEO --mtar ${BUILD_ARTIFACT} build
 
 [ $? != 0 ] && { echo "Cannot run mta docker image."; exit 1; }
 
-[ -f "${NODE_BUILD_ARTIFACT}" ] || { echo "Expected build result \"${NODE_BUILD_ARTIFACT}\" not found."; exit 1; }
-
-cd -
+[ -f "${NODE_TEST_FOLDER}/${BUILD_ARTIFACT}" ] || { echo "Expected build result \"${NODE_TEST_FOLDER}/${BUILD_ARTIFACT}\" not found."; exit 1; }
