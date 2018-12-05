3f8f2ec53a1879978ac6ab5edd6d5f32f10b1b46
Thorsten Duda <thorsten.duda@sap.com>
2018-12-05 19:16:43 +0100

    Dockerfile for node-rfc

---
+++ node-rfc/Gruntfile_ABAP.js
@@ -0,0 +1,219 @@
+"use strict";
+
+var rfc = require("node-rfc");
+var fs = require("fs");
+
+module.exports = function(grunt) {
+
+    // Project specific variables
+    var abapDevelopmentUser = process.env.ABAP_DEVELOPMENT_USER;
+    var abapDevelopmentPassword = process.env.ABAP_DEVELOPMENT_PASSWORD;
+    var abapDevelopmentServer = process.env.ABAP_DEVELOPMENT_SERVER;
+    var abapDevelopmentInstance = process.env.ABAP_DEVELOPMENT_INSTANCE;
+    var abapDevelopmentClient = process.env.ABAP_DEVELOPMENT_CLIENT;
+    var abapApplicationName = process.env.ABAP_APPLICATION_NAME;
+    var abapApplicationDesc = process.env.ABAP_APPLICATION_DESC;
+    var abapPackage = process.env.ABAP_PACKAGE;
+    var jobURL = process.env.JOB_URL;
+    var nexusSnapshotRepoURL = process.env.NEXUS_SNAPSHOT_REPO;
+    var gitCommit = process.env.GIT_COMMIT;
+
+    // Global Variables
+    var targetDir = "target";
+    var zipFileSuffix = "-opt-static-abap.zip";
+    var ctsDataFile = targetDir + "/CTS_Data.txt";
+    var nexusGroupId = "com.yourcompany";
+
+    // Project configuration.
+    var abapConn = {
+        user: abapDevelopmentUser,
+        passwd: abapDevelopmentPassword,
+        ashost: abapDevelopmentServer,
+        sysnr: abapDevelopmentInstance,
+        client: abapDevelopmentClient
+    };
+    grunt.initConfig({
+        pkg: grunt.file.readJSON("package.json"),
+        createTransportRequest: {
+            options: {
+                conn: abapConn,
+                author: abapDevelopmentUser,
+                description: "Commit: " + gitCommit
+            }
+        },
+        uploadToABAP: {
+            options: {
+                conn: abapConn,
+                zipFile: targetDir + "/<%= pkg.name %>" + zipFileSuffix,
+                zipFileURL: nexusSnapshotRepoURL + "/" + nexusGroupId.replace(/\./g, "/") + "/<%= pkg.name %>/<%= pkg.version %>-SNAPSHOT/<%= pkg.name %>-<%= pkg.version %>-SNAPSHOT.zip",
+                codePage: "UTF8"
+            }
+        },
+        releaseTransport: {
+            options: {
+                conn: abapConn
+            }
+        }
+    });
+
+    var rfcConnect = function(functionModule, importParameters, gruntContext) {
+        return new Promise(function(resolve, reject) {
+            var conn = gruntContext.options().conn;
+            var client = new rfc.Client(conn);
+
+            grunt.log.writeln("RFC client lib version:", client.version);
+
+            client.connect(function(err) {
+                if (err) { // check for login/connection errors
+                    grunt.log.errorlns("could not connect to server", err);
+                    return reject();
+                }
+                // invoke remote enabled ABAP function module
+                grunt.log.writeln("Invoking function module", functionModule);
+                client.invoke(functionModule,
+                    importParameters,
+                    function(err, res) {
+                        if (err) { // check for errors (e.g. wrong parameters)
+                            grunt.log.errorlns("Error invoking", functionModule, err);
+                            return reject();
+                        }
+                        client.close();
+                        grunt.log.writeln("Messages:", res.EV_LOG_MESSAGES);
+                        return resolve(res);
+                    });
+            });
+        });
+    };
+
+
+    grunt.registerTask("createTransportRequest", "Creates an ABAP Transport Request", function() {
+        grunt.log.writeln("Creating Transport Request");
+        var importParameters = {
+            AUTHOR: this.options().author,
+            TEXT: this.options().description
+        };
+        var done = this.async();
+        rfcConnect("BAPI_CTREQUEST_CREATE", importParameters, this)
+            .then(
+            function(returnValue) {
+                if (returnValue.EV_SUCCESS == "E" || returnValue.EV_SUCCESS == "W") {
+                    grunt.log.errorlns("Error invoking BAPI_CTREQUEST_CREATE.");
+                    grunt.log.errorlns("Message Id:", returnValue.EV_MSG_ID);
+                    grunt.log.errorlns("Message No:", returnValue.EV_MSG_NO);
+                    grunt.log.errorlns("Messages:", returnValue.EV_LOG_MESSAGES);
+                    done(false);
+                    return;
+                }
+                if (returnValue.REQUESTID == "") {
+                    grunt.log.errorlns("Error invoking BAPI_CTREQUEST_CREATE.");
+                    grunt.log.errorlns("Transport request could not be created.");
+                    grunt.log.errorlns(returnValue.RETURN.MESSAGE);
+                    done(false);
+                    return;
+                }
+                grunt.log.writeln("Transport request", returnValue.REQUESTID, "created.");
+                if (fs.existsSync(targetDir) === false) {
+                    fs.mkdirSync(targetDir);
+                }
+                fs.writeFile(ctsDataFile,
+                    JSON.stringify(
+                        { REQUESTID: returnValue.REQUESTID }
+                    ),
+                    function(err) {
+                        if (err) {
+                            grunt.log.errorlns("Error Creating file:", err);
+                            done(false);
+                            return;
+                        }
+                        grunt.log.writeln("Created file:", ctsDataFile);
+                        done();
+                    }
+                )
+            },
+            function() {
+                done(false);
+            });
+    });
+
+    grunt.registerTask("uploadToABAP", "Uploads the application to the ABAP System", function(transportRequest) {
+        grunt.log.writeln("Uploading to ABAP");
+        if (!transportRequest) {
+            if (!fs.existsSync(ctsDataFile)) {
+                grunt.log.errorlns("No Transport request specified. Pass one explicitly or run createTransportRequest first.");
+                return (false);
+            }
+            transportRequest = JSON.parse(fs.readFileSync(ctsDataFile, { encoding: "utf8" })).REQUESTID;
+        }
+        grunt.log.writeln("Transport request:", transportRequest);
+        var url = "";
+        if (!(typeof this.options().zipFile === "undefined") && fs.existsSync(this.options().zipFile)) {
+            url = jobURL + "/ws/" + this.options().zipFile;
+        }
+        else {
+            url = this.options().zipFileURL;
+        }
+        var importParameters = {
+            IV_URL: url,
+            IV_SAPUI5_APPLICATION_NAME: abapApplicationName,
+            IV_SAPUI5_APPLICATION_DESC: abapApplicationDesc,
+            IV_PACKAGE: abapPackage,
+            IV_WORKBENCH_REQUEST: transportRequest,
+            IV_TEST_MODE: "-",
+            IV_EXTERNAL_CODE_PAGE: this.options().codePage
+        };
+        var done = this.async();
+        grunt.log.writeln("Uploading application from", url);
+        rfcConnect("/UI5/UI5_REPOSITORY_LOAD_HTTP", importParameters, this)
+            .then(
+            function(returnValue) {
+                if (returnValue.EV_SUCCESS == "E" || returnValue.EV_SUCCESS == "W") {
+                    grunt.log.errorlns("Error invoking", "/UI5/UI5_REPOSITORY_LOAD_HTTP");
+                    grunt.log.errorlns("Message Id:", returnValue.EV_MSG_ID);
+                    grunt.log.errorlns("Message No:", returnValue.EV_MSG_NO);
+                    grunt.log.errorlns("Messages:", returnValue.EV_LOG_MESSAGES);
+                    done(false);
+                    return;
+                }
+                grunt.log.writeln("Application uploaded.");
+                done();
+            },
+            function() {
+                done(false);
+            });
+    });
+
+    grunt.registerTask("releaseTransport", "Releases an ABAP Transport Request", function(transportRequest) {
+        grunt.log.writeln("Releasing Transport Request");
+        if (!transportRequest) {
+            if (!fs.existsSync(ctsDataFile)) {
+                grunt.log.errorlns("No Transport request specified. Pass one explicitly or run createTransportRequest first.");
+                return (false);
+            }
+            transportRequest = JSON.parse(fs.readFileSync(ctsDataFile, { encoding: "utf8" })).REQUESTID;
+        }
+        grunt.log.writeln("Transport request:", transportRequest);
+        var importParameters = {
+            REQUESTID: transportRequest,
+            COMPLETE: "X",
+            BATCH_MODE: "X"
+        }
+        var done = this.async();
+        rfcConnect("BAPI_CTREQUEST_RELEASE", importParameters, this)
+            .then(
+            function(returnValue) {
+                if (returnValue.EV_SUCCESS == "E" || returnValue.EV_SUCCESS == "W") {
+                    grunt.log.errorlns("Error invoking", "BAPI_CTREQUEST_RELEASE");
+                    grunt.log.errorlns("Message Id:", returnValue.EV_MSG_ID);
+                    grunt.log.errorlns("Message No:", returnValue.EV_MSG_NO);
+                    grunt.log.errorlns("Messages:", returnValue.EV_LOG_MESSAGES);
+                    done(false);
+                    return;
+                }
+                grunt.log.writeln("Transport request released.");
+                done();
+            },
+            function() {
+                done(false);
+            });
+    });
+};
---
+++ node-rfc/README.md
@@ -0,0 +1,70 @@
+# RFC CTS+ Docker File
+## Description
+To bring your SAP Fiori application to your ABAP Front End Server you always need to create a transport request and upload the zip file to it. If you want to automate your delivery pipeline for this scenario, and you have your NW ABAP < 7.50 SP08, you can only do it by using RFC communication. But to get it run you need to do some installation and configuration steps.
+A detailed scenario description can be found in our [best practice guid][bestpractice].
+
+This Dockerfile provides an easy and consumable way for the node-rfc wrapper and the SAP NetWeaver RFC Library. The only thing you need to have is a container environment like docker to build and to execute the node-rfc wrapper image. You can use it standalone in your CI/CD environment or you can use it within our Jenkins shared library [project "Piper"][piper].
+
+## Requirements
+
+* General requirements can be found at the [repository readme][general]
+* An S-User for [SAP ONE][sapone]
+* Please Download SAPCAR for Linux on x86_64 from [SAP ONE][sapone]
+* Please Download NWRFC library ```SAP NW RFC SDK 7.20``` for Linux on x86_64 from [SAP ONE][sapone]
+* A local docker image repository
+
+## How to build it
+
+You can [build][dockerbuild] your docker image in different ways. 
+
+### Build Arguments
+| Argument | Description |
+| ---------| ------------|
+| **SAPCAR_FILE** |Path to SAPCAR file |
+| **NWRFC_FILE** | Path to your NWRFC SAR file |
+
+The build arguments can be a local path or an URL. Please consider the rules for the build context and the used [ADD][dockerbuildadd] command.
+
+Example: Build it with local file system context. Before, you have to clone this repository and copy the SAPCAR and  NWRFC SAR file
+into this directory.
+```
+docker build --build-arg SAPCAR_FILE=SAPCAR_1110-80000935.EXE --build-arg NWRFC_FILE=NWRFC_49-20004565.SAR .
+```
+
+Example: Build it with URLs
+```
+docker build --build-arg SAPCAR_FILE=https://<repoURL>/SAPCAR --build-arg NWRFC_FILE=https://<repoURL>/NWRFC.SAR --file Dockerfile myrepo.git#:node-rfc
+```
+
+## How to execute it
+
+### Environment Variables
+| Variable | Descritpion |
+| -------- | ----------- |
+| **ABAP_DEVELOPMENT_USER** | ABAP user to access RFC |
+| **ABAP_DEVELOPMENT_PASSWORD** | ABAP user password  |
+| **ABAP_DEVELOPMENT_SERVER** | Application server URL |
+| **ABAP_DEVELOPMENT_INSTANCE** | ABAP Instance ID |
+| **ABAP_DEVELOPMENT_CLIENT** | ABAP Client ID |
+| **ABAP_APPLICATION_NAME** | ABAP Application name |
+| **ABAP_APPLICATION_DESC** | ABAP Application description |
+| **ABAP_PACKAGE** | ABAP package name |
+| **JOB_URL**      |  Back reference of calling job |
+| **NEXUS_SNAPSHOT_REPO** | Intermediate nexus repo |
+| **GIT_COMMIT** | git commit id |
+
+Run it with:
+
+```
+docker run -v <local path>:/opt/sap/target --env <Envornment variables> <image> cts createTransportRequest|uploadToABAP|releaseTransport
+```
+You have to mount the location of the zip file into the container to /opt/sap/target. For more details about intermediate files please refer to our [best practices][bestpractice].
+
+[piper]: https://sap.github.io/jenkins-library/
+[noderfc]: https://sap.github.io/node-rfc/install.html
+[sapone]: https://launchpad.support.sap.com/
+[bestpractice]: https://developers.sap.com/tutorials/ci-best-practices-fiori-abap.html
+[general]: https://github.com/SAP/devops-docker-images/blob/master/README.md
+[dockerbuild]: https://docs.docker.com/engine/reference/commandline/build/
+[dockerbuildadd]: https://docs.docker.com/engine/reference/builder/#add
+[dockerrun]: https://docs.docker.com/engine/reference/run/
