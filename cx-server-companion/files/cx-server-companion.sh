7c68b27759140cd424e2003297d52725c6ec0977
Florian Wilhelm <florian.wilhelm02@sap.com>
2019-02-13 10:11:48 +0100

    fix

--- cx-server-companion/files/cx-server-companion.sh
+++ cx-server-companion/files/cx-server-companion.sh
@@ -592,7 +592,8 @@ function start_jenkins_container()
             environment_variable_parameters+=(-e CASC_JENKINS_CONFIG=/var/cx-server/jenkins-configuration)
         fi
 
-        for var in $(env | grep ^CX) do
+        for var in $(env | grep ^CX)
+        do
             environment_variable_parameters+=(-e $var)
         done
 
