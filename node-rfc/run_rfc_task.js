9686396ad301b06e1b4921fdb93a9243ca3526e5
Marcus Holl <marcus.holl@sap.com>
2019-02-15 13:28:04 +0100

    Improve check for return value for uploading content.
    Up to now we checked only for "W" and "E", but there was no explicit
    check for "S".
    
    I checked in ABAP backend, these 3 values are the only supported values,
    at least at the moment (status_succes, status_warning, status_error).

--- node-rfc/run_rfc_task.js
+++ node-rfc/run_rfc_task.js
@@ -160,9 +160,14 @@ module.exports = function(grunt) {
                     grunt.log.writeln("Return:", returnValue);
                     done(false);
                     return;
+                } else if (returnValue.EV_SUCCESS == 'S') {
+                    grunt.log.writeln("Application uploaded.");
+                    done();
+                } else {
+                    grunt.log.writeln("Invalid return status (EV_SUCCESS): " + returnValue.EV_SUCCESS);
+                    done(false);
+                    return;
                 }
-                grunt.log.writeln("Application uploaded.");
-                done();
             },
             function() {
                 done(false);
