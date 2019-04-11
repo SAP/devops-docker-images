2fc7141c5721cd6cae3b884eda52c976caf5b0d3
Florian Wilhelm <florian.wilhelm02@sap.com>
2019-04-11 17:43:41 +0200

    fix unzip flag

--- jenkins-master/tests/run_tests.sh
+++ jenkins-master/tests/run_tests.sh
@@ -3,8 +3,8 @@
 cd /var/jenkins_home
 
 curl -O https://repo.jenkins-ci.org/releases/io/jenkins/jenkinsfile-runner/jenkinsfile-runner/1.0-beta-7/jenkinsfile-runner-1.0-beta-7-app.zip && \
-unzip jenkinsfile-runner-1.0-beta-7-app.zip -q && \
-mkdir app && unzip /usr/share/jenkins/jenkins.war -q -d app/jenkins
+unzip -q jenkinsfile-runner-1.0-beta-7-app.zip && \
+mkdir app && unzip -q /usr/share/jenkins/jenkins.war -d app/jenkins
 
 bin/jenkinsfile-runner \
         --jenkins-war app/jenkins \
